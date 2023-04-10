4690a25c082c109faa94ed54f916c673c075413d
Chikiet12 <duongchikiet1812@gmail.com>
2023-04-10 11:37:43 +0700

    Pull Model

--- LTWNC/Controllers/ProductController.cs
+++ LTWNC/Controllers/ProductController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LTWNC.Models;
 namespace LTWNC.Controllers
 {
     public class ProductController : Controller
     {
+        LTWNCEntities1 database = new LTWNCEntities1();
         // GET: Product
-        public ActionResult Shop()
+        public ActionResult Shop(int? page, string SearchString = "")
         {
-            return View();
+            int pageSize = 4;
+            int pageNum = (page ?? 1);
+            if (SearchString != "")
+            {
+                var listProducts = database.SANPHAMs.Where(s => s.TENSP.Contains(SearchString)).ToList();
+                return View(listProducts.ToPagedList(pageNum, pageSize));
+            }
+            else
+            {
+                var listProducts = database.SANPHAMs.OrderByDescending(sp => sp.TENSP).ToList();
+                return View(listProducts);
+            }
         }
     }
 }
--- LTWNC/Models/LTWNC.Designer.cs
+++ LTWNC/Models/LTWNC.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Learning\HK6\LTWNC\DoAnCuoiKy\LTWNC\LTWNC\Models\LTWNC.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\kiet\hk6\Web năng cao(lt)\Đồ án\code\LTWNC\LTWNC\Models\LTWNC.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
