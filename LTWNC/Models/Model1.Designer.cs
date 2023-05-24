99c4950d3380c6afa71a31afaf51ba5c8781589f
Chikiet12 <duongchikiet1812@gmail.com>
2023-05-24 21:20:18 +0700

    Update Lấy danh mục

--- LTWNC/Models/Model1.Designer.cs
+++ LTWNC/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\giang\Documents\Project\Github\LTWNC\LTWNC\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\kiet\hk6\Web năng cao(lt)\Đồ án\code\LTWNC\LTWNC\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
